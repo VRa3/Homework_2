84806b6bc9f6fc21a019493cb67ce61827611338
Vra3 <vrafal333@gmail.com>
2018-05-23 12:43:44 +0200

    improved hover event in faq

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(".panel__questionWrapper").click(function(){
   $(this).closest(".panel__wrapper").toggleClass("active");
   $(this).siblings(".panel__arrow").toggleClass("rotated");
   $(this).children(".panel__counter").toggleClass("red");
+  $(this).parents().eq(3).removeClass("hovered");
 });
 
 $(".panel__arrow").click(function(){
@@ -10,8 +11,17 @@ $(".panel__arrow").click(function(){
   $(this).siblings(".panel__questionWrapper").children(".panel__counter").toggleClass("red");
 });
 
-$(".panel__questionWrapper").hover(function(){
-  if ($(this).not(".active")){
-      $(this).closest(".panel__wrapper").toggleClass("hovered");
+$(".panel__questionWrapper").mouseenter(function(){
+  if ($(this).parents(".panel__wrapper").hasClass("active")){
+    return;
+  } else{
+    $(this).parents().eq(3).addClass("hovered");
   }
 });
+
+$(".panel__questionWrapper").mouseleave(function(){
+  $(this).parents().eq(3).removeClass("hovered");
+});
+
+
+
